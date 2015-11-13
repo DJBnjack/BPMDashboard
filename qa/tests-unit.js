70f31595e3d6056bcffeca1a4dd6995f23a8280e
Johannes Bertens <johannes.bertens@live.nl>
2015-11-13 23:02:52 +0100

    Initialized Express and Node pretty empty project.

---
+++ qa/tests-unit.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+suite('UnitTests', function() {
+	test('this will succeed', function(){
+		expect(true).to.equal(true);
+	});
+	
+	// test('this will fail', function(){
+	// 	expect(true).to.equal(false);
+	// });
+});
